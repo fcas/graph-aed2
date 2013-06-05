615e2cb6f94dae9e3d73321b7e0940ef3718a7f2
F3L1NUX@gmail.com <F3L1NUX@gmail.com@0e233613-3605-c6a9-d8bd-29160f0d5ac7>
2013-06-05 09:54:31 +0000

    Classe auxiliares para calcular o menor caminho. Modificação da classe Aresta, adicionando gets and setters e o atributo peso.

--- src/grafo/Aresta.java
+++ src/grafo/Aresta.java
@@ -3,15 +3,43 @@ package grafo;
 public class Aresta {
 	
 	//Vértice adjacente
-	public Vertice vertice; 
+	private Vertice vertice; 
 	
 	//Próxima aresta na lista de adjacência
-	public Aresta proximo;
+	private Aresta proximo;
+	
+	private int peso;
 
 	public Aresta(Vertice vertice, Aresta sucessor){ 
 		this.vertice = vertice; 
 		this.proximo = sucessor; 
 	}
+
+	public Vertice getVertice() {
+		return vertice;
+	}
+
+	public void setVertice(Vertice vertice) {
+		this.vertice = vertice;
+	}
+
+	public Aresta getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(Aresta proximo) {
+		this.proximo = proximo;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	
 	
 	
 }
